0c83d3de920e57e355f2ceb49166237ecee8cb08
yberfox228 <domvlielupe3@gmail.com>
2018-08-21 17:47:22 +0300

    Improvs
    Addition of new js and grid layout

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const numberPad = document.querySelector('.numberPad');
+//Getting DOM objects
+const output = document.querySelector('.display');
+const buttons = document.body.querySelectorAll('.buttons');
 
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
+//Assigning variables
+var input = '';
+var unknownNumber = 0;
+
+//
+function getNewNumber() {
+  unknownNumber = Math.floor(Math.random() * 100);
 }
+
+/*
+document.querySelector('html').onclick = function() {
+  alert('Ouch! Stop poking me!');
+  output.innerHTML = "Heya";
+};
+*/
